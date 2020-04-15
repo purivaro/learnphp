d142ae85182b4eacf64e2562496f5b5e7e89e126
Purivaro <aun.puri@gmail.com>
2020-04-15 17:05:18 +0700

    HTML ep.03

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" href="https://s.isanook.com/ho/0/rp/rc/w525h296/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL2hvLzAvdWQvMTEvNTk1MDEvdG9yLmpwZw==.jpg">
+	<title>Basic HTML</title>
+</head>
+<body>
+	<section>
+		<h1 style="background: yellow;">บทสวดมนต์ก่อนนอน บทแผ่เมตตา อานิสงส์ของการสวดมนต์</h1><h1 style="background:cyan;">อยู่ต่อได้ไหม</h1>
+		<hr>
+		<img src="img/photo/chanting.jpg" width="500">
+		<img src="/learnphp/img/photo/chanting.jpg" width="500">
+		<img src="/learnphp/img/contact.png">
+		<!-- <h2>อันนี้คือ h2</h2> -->
+		<p>
+			บทสวดมนต์ก่อนนอน นับเป็นวิธีหนึ่ง<br>
+			ที่ทำให้เรา&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;      มีความสุขขึ้นได้ 
+			โดยที่ในแต่ละวันเราต้องพบเจอกับเรื่องราวมากมาย<br>
+			ทั้งเรื่องที่ทำให้ชีวิตเป็นสุข ไปจนถึงเรื่องราวแย่ๆ ที่ทำให้ชีวิตวุ่นวาย<br> 
+			เมื่อกลับถึงบ้านเราต่างก็ต้องการเวลาพักผ่อน อยากนอนหลับสบายเพื่อลืมเรื่องราวแย่ๆ
+		</p>
+		<a href="page/contact.php">ไปหน้าcontact1</a>
+		<a href="/learnphp/page/contact.php">ไปหน้าcontact2</a>
+	</section>
+	<section>
+		<h3>อานิสงส์จากการสวดมนต์</h3>
+		<ol>
+			<li>สวดมนต์เพื่อถวายเป็นพุทธบูชาเป็นบุญที่ได้กล่าวคำศักดิ์สิทธิ์ที่พระพุทธเจ้าบัญญัติไว้</li>
+			<li>เกิดผลดีต่อร่างกาย</li>
+			<li>เป็นการบำเพ็ญภาวนาอย่างหนึ่ง</li>
+		</ol>
+		<h3>บทสวดมนต์ บทแผ่ส่วนกุศล</h3>
+		<ul>
+			<li>อิทัง เม มาตาปิตูนัง โหตุ สุขิตา โหนตุ มาตาปิตะโร</li>
+			<li>อิทัง เม ญาตีนัง โหตุ สุขิตา โหนตุ ญาตะโย</li>
+			<li>อิทัง เม คุรูปัชฌายาจริยานัง โหตุ สุขิตา โหนตุ คุรูปัชฌายาจริยา</li>
+		</ul>
+	</section>
+	<table>
+		<thead>
+			<tr>
+				<th>ชื่อ</th>
+				<th>นามสกุล</th>
+				<th>อายุ</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>เอก</td>
+				<td>ทองดี</td>
+				<td>20</td>
+			</tr>
+			<tr>
+				<td>เตย</td>
+				<td>ใจดี</td>
+				<td>18</td>
+			</tr>
+			<tr>
+				<td>แจน</td>
+				<td>ใสปิ๊ง</td>
+				<td>23</td>
+			</tr>
+		</tbody>
+	</table>
+	<hr>
+</body>
+</html>
---
+++ page/contact.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Contact</title>
+</head>
+<body>
+	<h1>Contact</h1>
+	<img src="../img/contact.png">
+	<img src="/learnphp/img/contact.png">
+	<a href="../index.php">ย้อนกลับไปหน้าแรก</a>
+	<a href="/learnphp/index.php">ย้อนกลับไปหน้าแรก</a>
+</body>
+</html>
