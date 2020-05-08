4a6bd726d608c6236253d3732e15a4a275ca8d44
Purivaro <aun.puri@gmail.com>
2020-05-08 22:37:50 +0700

    CSS Flexbox ep.07

---
+++ 04_css/05_flex.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Flex</title>
+	<link rel="stylesheet" href="css/flex.css">
+</head>
+<body>
+	<div class="menu-container">
+		<a href="01_basic.php">Basic</a>
+		<a href="03_box.php">Box</a>
+		<a href="04_position.php">Position</a>
+		<a href="05_flex.php">Flex</a>
+	</div>
+
+	<div class="container">
+		<div class="item" id="i1">Item1</div>
+		<div class="item" id="i2">Item2</div>
+		<div class="item" id="i3">Item3</div>
+		<div class="item" id="i4">Item4</div>
+		<!-- <div class="item">Item5</div>
+		<div class="item">Item6</div>
+		<div class="item">Item7</div>
+		<div class="item">Item8</div>
+		<div class="item">Item9</div> -->
+	</div>
+
+	<div class="login-container">
+		<div class="login-box">
+			<form>
+				<h2>Login</h2>
+				<div class="form-row">
+					<label>User</label>
+					<input type="text" name="user">
+				</div>
+				<div class="form-row">
+					<label>Password</label>
+					<input type="password" name="user">
+				</div>
+				<div class="form-row">
+					<button type="submit" class="btn-submit">Login</button>
+				</div>
+			</form>
+		</div>
+	</div>
+
+	<div class="news-container">
+		<div class="news-column">
+			<img src="img/med1.jpg" alt="">
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus recusandae corrupti quae adipisci, eos repellat dolores. Dicta veritatis nesciunt temporibus architecto, consequatur id. Nobis voluptates tempore sed! In nobis facere, molestias fugiat dolor cupiditate sed debitis suscipit pariatur reprehenderit adipisci consequuntur. Obcaecati magnam deserunt, dolor ipsum cumque magni dolorum adipisci totam tempore facilis? Exercitationem, ex obcaecati! Repudiandae qui porro obcaecati recusandae reiciendis fugiat perferendis illum dolor ex repellat. Provident consectetur illum, iste ducimus, accusamus incidunt eius unde in, possimus minima voluptates? Fugiat aut voluptates placeat illo vitae quam aperiam a qui assumenda, laboriosam animi hic magnam deserunt id recusandae. Ad.</p>
+		</div>
+		<div class="news-column" id="news2">
+			<img src="img/med2.jpg" alt="">
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus recusandae corrupti quae adipisci, eos repellat dolores. Dicta veritatis nesciunt temporibus architecto, consequatur id. Nobis voluptates tempore sed! In nobis facere, molestias fugiat dolor cupiditate sed debitis suscipit pariatur reprehenderit adipisci consequuntur. Obcaecati magnam deserunt, dolor ipsum cumque magni dolorum adipisci totam tempore facilis? Exercitationem, ex obcaecati! Repudiandae qui porro obcaecati recusandae reiciendis fugiat perferendis illum dolor ex repellat. Provident consectetur illum, iste ducimus, accusamus incidunt eius unde in, possimus minima voluptates? Fugiat aut voluptates placeat illo vitae quam aperiam a qui assumenda, laboriosam animi hic magnam deserunt id recusandae. Ad.</p>
+		</div>
+		<div class="news-column">
+			<img src="img/med3.jpg" alt="">
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus recusandae corrupti quae adipisci, eos repellat dolores. Dicta veritatis nesciunt temporibus architecto, consequatur id. Nobis voluptates tempore sed! In nobis facere, molestias fugiat dolor cupiditate sed debitis suscipit pariatur reprehenderit adipisci consequuntur. Obcaecati magnam deserunt, dolor ipsum cumque magni dolorum adipisci totam tempore facilis? Exercitationem, ex obcaecati! Repudiandae qui porro obcaecati recusandae reiciendis fugiat perferendis illum dolor ex repellat. Provident consectetur illum, iste ducimus, accusamus incidunt eius unde in, possimus minima voluptates? Fugiat aut voluptates placeat illo vitae quam aperiam a qui assumenda, laboriosam animi hic magnam deserunt id recusandae. Ad.</p>
+		</div>
+	</div>
+</body>
+</html>
