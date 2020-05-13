a27394788819bae63fbfc09f42adffaabd5560b1
Purivaro <aun.puri@gmail.com>
2020-05-13 11:19:47 +0700

    Bootstrap ep.09

--- 04_css/06_bst.php
+++ 04_css/06_bst.php
@@ -11,6 +11,36 @@
 		<h1 class="border border-primary text-success bg-danger d-block p-3 text-right">Hello Bootstrap</h1>
 	</section>
 
+	<section class="min-vh-100 bg-light">
+		<div class="container pt-5">
+			<div class="row">
+				<div class="col-4 bg-dark text-white"><h2>Col-4</h2></div>
+				<div class="col-4"><h2>Col-4</h2></div>
+				<div class="col-4 bg-dark text-white"><h2>Col-4</h2></div>
+			</div>
+			<div class="row mt-5">
+				<div class="col-6 bg-success"><h2>Col-6</h2></div>
+				<div class="col-4 bg-primary"><h2>Col-4</h2></div>
+				<div class="col bg-warning"><h2>Col</h2></div>
+			</div>
+			<div class="row mt-5">
+				<div class="col-8 bg-info">
+					<h2>Col-8</h2>
+					<div class="row">
+						<div class="col bg-warning"><h2>Col-6</h2></div>
+						<div class="col bg-danger"><h2>Col-6</h2></div>
+					</div>
+				</div>
+				<div class="col-4 bg-success"><h2>Col-4</h2></div>
+			</div>
+			<div class="row mt-5">
+				<div class="col-sm-6 col-md-3 col-lg-8 col-xl-2 bg-success"><h2>col-sm-6 col-md-3 col-lg-8 col-xl-2</h2></div>
+				<div class="col-sm-6 col-md-9 col-lg-4 col-xl-10 bg-primary"><h2>col-sm-6 col-md-9 col-lg-4 col-xl-10</h2></div>
+			</div>
+		</div>
+	</section>
+
+
 
 	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
