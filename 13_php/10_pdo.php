1979efcdfe93a8a1752a817e2c96c0389d6d4400
Purivaro <aun.puri@gmail.com>
2020-07-10 14:55:08 +0700

    PHP SQL Ep.25

--- 13_php/10_pdo.php
+++ 13_php/10_pdo.php
@@ -14,13 +14,6 @@
 				<div class="card mb-3">
 					<h4 class="card-header bg-primary text-white">PHP PDO</h4>
 					<div class="card-body">
-						<form action="">
-							<div class="form-group">
-								<label for="">ค้นหา</label>
-								<input type="text" name=":search" class="form-control" value="<?php echo $_REQUEST[':search'];?>">
-							</div>
-							<button type="submit" class="btn btn-primary">Search</button>
-						</form>
 							<?php
 								use App\Model\Person;
 								$personObj = new Person();
@@ -30,23 +23,6 @@
 								foreach($persons as $person) {
 									echo "<p>{$person['id']} {$person['firstname']} {$person['nickname']}</p>";
 								}
-
-								echo "<h2>Update Person</h2>";
-								$personObj->updatePerson([":firstname"=>"มินตรา", ":nickname"=>"BlackPink", ":dob"=>"208-04-22
-								",":gender_id"=>"2", ":id"=>"16"]);
-
-								// echo "<h2>Add New Person</h2>";
-								// $person3_id = $personObj->addPerson([":firstname"=>"มินตรา", ":nickname"=>"BlackPink", ":dob"=>"2008-06-16"]);
-								// echo "<h3>New Person Id : {$person3_id}</h3>";
-
-								echo "<h2>Filtered Persons</h2>";
-								$persons2 = $personObj->getPersons($_REQUEST);
-								foreach($persons2 as $person) {
-									echo "<p>{$person['id']} {$person['firstname']} {$person['nickname']} {$person['dob']}</p>";
-								}
-								
-								echo "<h2>Delete Person</h2>";
-								$personObj->deletePerson(15);
 							?>
 					</div>
 				</div>
--- src/Model/Person.php
+++ src/Model/Person.php
@@ -11,48 +11,4 @@ class Person extends Db {
 		return $data;
 	}
 
-	public function getPersons($search=null) {
-		$filters = [];
-
-		if($search[':search']) {
-			$where .= "
-				AND (
-					firstname LIKE :search
-					OR nickname LIKE :search
-				)
-			";
-			$filters[":search"] = "%{$search[':search']}%";
-		}
-
-		$sql = "SELECT * FROM persons WHERE 1=1 {$where} ";
-		$stmt = $this->pdo->prepare($sql);
-		$stmt->execute($filters);
-		return $stmt->fetchAll();
-	}
-
-	public function addPerson($person) {
-		$sql = "INSERT INTO persons (firstname, nickname, dob) VALUES (:firstname, :nickname, :dob)";
-		$stmt = $this->pdo->prepare($sql);
-		$stmt->execute($person);
-		return $this->pdo->lastInsertId();
-	}
-
-	public function updatePerson($person) {
-		$sql = "UPDATE persons SET 
-			firstname = :firstname, 
-			nickname = :nickname, 
-			dob = :dob, 
-			gender_id = :gender_id 
-			WHERE id = :id
-		";
-		$stmt = $this->pdo->prepare($sql);
-		$stmt->execute($person);
-	}
-
-	public function deletePerson($id) {
-		$sql = "DELETE FROM persons WHERE id = ?";
-		$stmt = $this->pdo->prepare($sql);
-		$stmt->execute([$id]);
-	}
-
 }
